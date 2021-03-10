55fc06c952e25442d10e1ff77b8656ba1e62a37a
Yuendel Farias <yuendel.fb@hotmail.com>
2021-03-09 21:42:53 -0300

    Nível fácil, para testar nosso entendimento de booleano

---
+++ jackpot.js
@@ -0,0 +1,11 @@
+function solucao(resultado1, resultado2, resultado3) {
+	// seu código aqui	
+    
+    let resultado;
+    if (resultado1 === resultado2 && resultado1 === resultado3){
+        resultado = true;
+    } else{
+        resultado = false;
+    }
+    console.log(resultado);
+}
