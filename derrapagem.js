3feff439a7e43c5a088c40d2d4cd6babb2f8e160
Yuendel Farias <yuendel.fb@hotmail.com>
2021-03-09 21:41:42 -0300

    Outro de nível fácil, ja nos dando a formula e particulamente divertido de fazer

---
+++ derrapagem.js
@@ -0,0 +1,6 @@
+function solucao(velocidade, aceleracao) {
+	//seu código aqui
+    let distanciaPercorrida = (-(velocidade*velocidade)) / (aceleracao * 2);
+    console.log(distanciaPercorrida);
+  
+}
